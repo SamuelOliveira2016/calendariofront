2a370bd74b9c5b57a66eaed30490d4001863791b
igorcacerez <igor.cacerez@gmail.com>
2024-02-15 11:13:22 -0300

    Layout pagina do calendario

--- src/middleware/PrivateRoute.js
+++ src/middleware/PrivateRoute.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Navigate } from 'react-router-dom';
 
 export default ({ element: Component, ...rest }) => {
-    //const token = localStorage.getItem('access_token');
-    const token = "aa"
+    const token = localStorage.getItem('access_token');
+    //const token = "aa"
 
     // Verifica se não possui um token, caso não tenha redireciona para o login
     if (!token) return <Navigate to="/" replace />
--- src/pages/Calendario/AulaForm.js
+++ src/pages/Calendario/AulaForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import FormGroup from "../../components/Form/FormGroup";
 export default AulaForm; // Exportação padrão
 
 
@@ -25,15 +26,26 @@ function AulaForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <select value={selectedAula} onChange={e => setSelectedAula(e.target.value)}>
-                {console.log(aulas)}
-                <option value="">Selecione uma Aula</option>
-                {aulas.map(aula => (
-                    <option key={aula.id} value={aula.id}>{aula.curso_uc_professor.curso.nome}</option>
-                ))}
-            </select>
-            <button type="submit">Alocar Aula</button>
+        <form className="row" onSubmit={handleSubmit}>
+            <div className='col-md-12'>
+                <FormGroup
+                    type="select"
+                    id="busca"
+                    name="busca"
+                    label="Selecione uma Aula"
+                    value={selectedAula}
+                    setValue={setSelectedAula}
+                >
+                    <option value="">Selecione uma Aula</option>
+                    {aulas.map(aula => (
+                        <option key={aula.id} value={aula.id}>{aula.curso_uc_professor.curso.nome}</option>
+                    ))}
+                </FormGroup>
+            </div>
+
+            <div className={"col-md-12"}>
+                <button className="btn btn-primary" type="submit">Alocar Aula</button>
+            </div>
         </form>
     );
 }
